90604b372e8e4ab81930a083195bb358c48e39c7
K <2471306@cognizant.com>
2026-06-08 11:09:45 +0530

    include test user settings and add utility function to get user setting and its path operations also update utility function exit user and user setting iteration statement

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from fastapi.encoders import jsonable_encoder
 
 from pydantic import BaseModel
 
-from settings import settings
+from settings import settings, user_settings
 from users import users
 
 
@@ -34,13 +34,28 @@ class Users(BaseModel):
 
 
 def is_user_exit(id: int):
-    isExit = False
+    is_exit = False
     
     for user in users:
         if user["user_id"] == id:
-            isExit = True
+            is_exit = True
+            break
     
-    return isExit
+    return is_exit
+
+def get_user_settings(user_id: int):
+    is_setting_exit = False
+    config = None
+
+    for setting in user_settings:
+        if setting["user_id"] == user_id:
+            is_setting_exit = True
+            config = setting
+            break
+    return {"setting_exit": is_setting_exit, "config": config}
+    
+
+
             
 @app.exception_handler(RequestValidationError)
 async def validation_exception_handler(request: Request, exc: RequestValidationError):
@@ -71,3 +86,11 @@ async def get_users(user_id: int):
     if not is_user_exit(user_id):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="user not found")
     return JSONResponse(content=users[user_id - 1], status_code=status.HTTP_200_OK)
+
+@app.get("/users/{user_id}/settings")
+async def get_user_config(user_id: int):
+    user_config = get_user_settings(user_id)
+    if not user_config["config"]:
+        default_settings = { "setting_exit": user_config["setting_exit"], "default_settings": settings}
+        return JSONResponse(content=default_settings, status_code=status.HTTP_200_OK)
+    return JSONResponse(content=user_config["config"], status_code=status.HTTP_200_OK)
--- settings.py
+++ settings.py
@@ -8,7 +8,7 @@ settings = {
         },
         {
             "provider": "gemini",
-            "models": ["gemini-flash", "gemini-flast-2.5"]
+            "models": ["gemini-flash", "gemini-flash-2.5"]
         },
         {
             "provider": "anthropic",
@@ -16,3 +16,27 @@ settings = {
         }
     ]
 }
+
+user_settings= [
+    {
+        "user_id": 1,
+        "settings" : {
+            "provider" : "openai",
+            "models" : "gpt-5"
+        }
+    },
+    {
+        "user_id": 5,
+        "settings" : {
+            "provider" : "anthropic",
+            "models" : "opus"
+        }
+    },
+    {
+        "user_id": 3,
+        "settings" : {
+            "provider" : "gemini",
+            "models" : "gemini-flast-2.5"
+        }
+    },
+]
