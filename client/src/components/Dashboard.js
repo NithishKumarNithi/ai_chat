6c6a40eeaa7abab18f47624931ba78663baf64b2
Nithish Kumar Nithi <nithishkumarnithi19@gmail.com>
2026-06-19 20:48:39 +0530

    add settings section to the dashboard page

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -9,7 +9,7 @@ export default function Dashboard() {
   useEffect(() => {
     let authToken = getSession("authToken");
 
-    // if (!authToken) throw Error("Token is Missing");
+    if (!authToken) throw Error("Token is Missing");
 
     async function getUserData() {
       let userData = await fetch(`http://127.0.0.1:8000/users/${userId}`, {
@@ -19,13 +19,24 @@ export default function Dashboard() {
         },
       });
 
-      let response = await userData.json();
-      console.log(response);
-      if (!response) {
+      let userSettings = await fetch(
+        `http://127.0.0.1:8000/users/${userId}/settings`,
+        {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${authToken}`,
+          },
+        },
+      );
+
+      let userInfoRes = await userData.json();
+      let userSettingsRes = await userSettings.json();
+
+      if (!userInfoRes || !userSettingsRes) {
         throw Error("Invalid User Details");
       }
-
-      setUserInfo(response);
+      console.log({ ...userInfoRes, ...userSettingsRes });
+      setUserInfo({ ...userInfoRes, ...userSettingsRes });
     }
     getUserData();
   }, [userId]);
@@ -35,6 +46,11 @@ export default function Dashboard() {
       <h1>Dashboard for user </h1>
       <p>UserName : {userInfo.name ? userInfo.name : "-"}</p>
       <p>Email : {userInfo.email ? userInfo.email : "-"}</p>
+
+      <h2>User Settings </h2>
+
+      <p>Provider : {userInfo.settings ? userInfo.settings.provider : "-"}</p>
+      <p>Model : {userInfo.settings ? userInfo.settings.models : "-"}</p>
     </>
   );
 }
